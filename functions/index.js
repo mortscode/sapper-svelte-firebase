4facfc5368fa709d9b2b7271a3839b4abaf1363c
Scot Mortimer <scot.mortimer@gmail.com>
2019-06-26 15:30:59 -0700

    initial commit

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,4 @@ const express = require('express');
 
 // We have to import the built version of the server middleware.
 const { server } = require('./__sapper__/build/server/server');
-exports.ssr = functions
-	.region('us-central1')
-	.https.onRequest(server);
+exports.ssr = functions.region('us-central1').https.onRequest(server);
